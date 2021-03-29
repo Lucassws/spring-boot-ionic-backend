cb0432a4beb4516cfc121a764b56311639cde1fd
Mestreluck <lucasws.bass@gmail.com>
2021-03-28 22:52:25 -0300

    criando operacao de instanciacao

--- src/main/java/com/lucaswilliam/cursomc/CursomcApplication.java
+++ src/main/java/com/lucaswilliam/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.lucaswilliam.cursomc;
 
+import com.lucaswilliam.cursomc.domain.Categoria;
+import com.lucaswilliam.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	/*Método que Instancia 2 objetos iniciais para popular o banco de dados*/
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+
+	}
 }
