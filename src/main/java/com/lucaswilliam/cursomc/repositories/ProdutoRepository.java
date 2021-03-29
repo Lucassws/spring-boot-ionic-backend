621c58a316ac834778b5e0b506aac5e245d202c8
Mestreluck <lucasws.bass@gmail.com>
2021-03-28 23:25:43 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/lucaswilliam/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.lucaswilliam.cursomc.repositories;
+
+import com.lucaswilliam.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
