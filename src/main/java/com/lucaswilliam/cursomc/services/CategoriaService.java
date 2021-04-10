a793f2a4a476c7457ddb0f7ddacc0844122c72fc
Mestreluck <lucasws.bass@gmail.com>
2021-04-10 00:21:11 -0300

    Deletando uma categoria com DELETE e tratando deleção que possui produtos

--- src/main/java/com/lucaswilliam/cursomc/services/CategoriaService.java
+++ src/main/java/com/lucaswilliam/cursomc/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.lucaswilliam.cursomc.services;
 
 import com.lucaswilliam.cursomc.domain.Categoria;
 import com.lucaswilliam.cursomc.repositories.CategoriaRepository;
+import com.lucaswilliam.cursomc.services.exception.DataIntegrityException;
 import com.lucaswilliam.cursomc.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.Id;
@@ -30,6 +32,17 @@ public class CategoriaService {
     public Categoria update(Categoria obj){
         find(obj.getId());
         return repo.save(obj);
+    }
+
+    public void delete(Integer id){
+        find(id);
+        try{
+           repo.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+        }
+
     }
 
 }
---
+++ src/main/java/com/lucaswilliam/cursomc/services/exception/DataIntegrityException.java
@@ -0,0 +1,13 @@
+package com.lucaswilliam.cursomc.services.exception;
+
+public class DataIntegrityException extends RuntimeException{
+
+    public DataIntegrityException(String msg){
+        super(msg);
+    }
+
+    public DataIntegrityException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+}
