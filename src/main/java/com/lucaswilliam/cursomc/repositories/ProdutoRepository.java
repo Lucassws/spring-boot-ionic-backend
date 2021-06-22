d480ae2d85c3ae11f3e801d3c755c06d366e934a
Mestreluck <lucasws.bass@gmail.com>
2021-06-22 01:05:06 -0300

    Subtotal e Total do pedido

--- src/main/java/com/lucaswilliam/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/lucaswilliam/cursomc/repositories/ProdutoRepository.java
@@ -18,5 +18,6 @@ public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
     @Transactional(readOnly=true)
     @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
     Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param ("categorias") List<Categoria> categorias, Pageable pageRequest);
+    //PODE USAR APENAS O NOME COM NOTACOES SEM PRECISAR DA QUERY OU PARAM - findDistinctByNomeContainingAndCategoriasIn
 
 }
